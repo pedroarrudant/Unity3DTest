0b191a64790b4378f78b8df773a494a4ef89bb18
pedroarrudant <pedro.arruda@outlook.com.br>
2020-05-03 00:09:09 -0300

    Colocando start do projeto + começo dos scripts

---
+++ Assets/CharController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 4f;
+
+    Vector3 forward, right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
